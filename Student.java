da35f42a1f69c6c0b1a50afcae94adf36ab923a9
Mohamad Radaideh <117695265+momo674@users.noreply.github.com>
2023-07-06 10:25:10 -0400

    Added Getters for Course

--- Student.java
+++ Student.java
@@ -1,4 +1,19 @@
 public class Student {
-    private int [7][48] SCHEDULE;
-    
+    private int [][] SCHEDULE;
+    private String FIRST_NAME;
+    private String LAST_NAME;
+    private int ID;
+    private String PROGRAM_TITLE;
+
+    public Student(String fname, String lname, int id; String program){
+        this.FIRST_NAME = fname;
+        this.LAST_NAME = lname;
+        this.ID = id;
+        this.PROGRAM_TITLE = program;
+        this.SCHEDULE = new int[7][48];
+    }
+
+    public addCourse(Course c) {
+        
+    }
 }
